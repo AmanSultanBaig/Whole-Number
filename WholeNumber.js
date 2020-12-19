611d72b9b9544012b6e47501f11d30f31f293a86
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-12-19 11:51:23 +0500

    whole number logic updated

--- WholeNumber.js
+++ WholeNumber.js
@@ -7,3 +7,13 @@ function wholeNumber(n){
 }
 
 wholeNumber(2345)
+
+
+// another way to find whole number 
+
+function testWholeNumber(number) {
+	let result = (number - Math.floor(number)) !== 0;
+    result ? console.log("Decimal Number") : console.log("Whole Number Found")
+}
+
+testWholeNumber(129.9)
